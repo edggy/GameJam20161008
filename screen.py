b31f53eb778e6b5cbe8fd7af2ae2acb36e36506d
NicoCobb <cobbn@rpi.edu>
2016-10-09 00:32:56 -0400

    forgot to save

--- screen.py
+++ screen.py
@@ -10,22 +10,37 @@ Has scene on top, options on bottom
 class Screen(tk.Frame):
     def __init__(self, master = None, initBackground = None):
         tk.Frame.__init__(self, master)
-        self.state = state.State("settings.txt")
-        self.scene = scene.Scene(self,self.state.background, .75)
-        self.buttons = [Button(self, text=state.options[i], command=lambda: self.state.action(i)) for i in range(5)]
-        #self.button0 = Button(self, text=state.options[0], command=lambda: self.state.action(0))
-        #
+        self.currentState = state.State("settings.txt")
+        self.currentScene = scene.Scene(self,self.state.background, .75)
         
-        self.scene.pack(side = 'top')
-        for button in buttons:
+        #create the five necessary buttons for player choices and hide them
+        self.optionButtons = [Button(self, text=currentState.options[i], command=lambda: self.buttonPress(i)) for i in range(5)]
+        for button in optionButtons:
+            button.lower(self.currentScene.background)
+            
+        self.nextButton = Button(self, text='Continue', command=self.continueGame)
+        self.textBox = 
+        
+        self.currentScene.pack(side = 'top')
+        for button in optionButtons:
             button.pack(side = 'top')
         
-    def changeButtons():
         
+    def changeButtons(self, optionsList):
+        for i,option in enumerate(optionsList):
+            if i >= self.currentState.firstGrey:
+                self.optionButtons[i].text = option
+
+    #only relevant on say or opts because those are when the player needs time to read and then select continue
+    def continueGame(self):
+        if 'opt' == self.state.action():
+            
+
+        
+    def buttonPress(self, num):
         pass
-
-
-
+        
+        
 if __name__ == "__main__":
     root = tk.Tk()
     app = Screen(master=root)
