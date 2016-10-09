ca12a460c45578864df550a7fcf18e8933b0ecc8
edggy <edggy@Ethan-Alienware>
2016-10-08 21:53:42 -0400

    Added .strip()s

--- state.py
+++ state.py
@@ -12,16 +12,20 @@ class State:
             for line in f:
                 key, value = line.split(maxsplit=1)
                 if key in keys:
-                    setattr(self, key, value)
+                    setattr(self, key.strip(), value.strip())
         
         self.story = Story(self.storyFile)
         self.background = self.story.startBackground
+        self.speaker = ''
         self.text = ''
         self.options = []
         
     def action(self, actionNumber):
         for data in self.story[actionNumber]:
             pass
+        
+    def next(self):
+        pass
     
     
 
